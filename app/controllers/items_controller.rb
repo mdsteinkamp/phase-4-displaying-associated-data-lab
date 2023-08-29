467ff02896fe7afc4cecb39310a8dd822e650e29
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-08-28 19:59:46 -0700

    commit tree 786b73faa3ff8740ebe48c0f1dbcdedc9cfbf65b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+
+  def index 
+    items = Item.all.order(:price)
+    render json: items, include: :user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def show 
+    user = find_user
+    render json: user, include: :items
+  end
+
+  private
+
+  def find_user
+    user = User.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
 end
